af8be7d671756d19007afc7453a8c7c5c9748602
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-05-18 18:16:02 +0800

    refactor: update default options

--- package/src/consts/swc.ts
+++ package/src/consts/swc.ts
@@ -1,22 +1,32 @@
-import type { JsMinifyOptions, Options } from "@swc/core";
+import type {
+    JsMinifyOptions as MinifyOptions,
+    Options as TransformOptions,
+} from "@swc/core";
 
 const OPTIONS_TRANSFORM_DEFAULT = {
     jsc: {
-        target: "es5",
         parser: {
             syntax: "typescript",
             tsx: true,
             decorators: true,
         },
+        target: "es3",
+        keepClassNames: true,
+        preserveAllComments: true,
     },
     minify: false,
-} as const satisfies Options;
+    sourceMaps: true,
+} as const satisfies TransformOptions;
 
 const OPTIONS_MINIFY_DEFAULT = {
     compress: true,
     mangle: true,
-    ecma: 5,
+    ecma: 3,
+    keep_classnames: false,
+    keep_fnames: false,
     module: "unknown",
-} as const satisfies JsMinifyOptions;
+    safari10: true,
+    sourceMap: true,
+} as const satisfies MinifyOptions;
 
 export { OPTIONS_MINIFY_DEFAULT, OPTIONS_TRANSFORM_DEFAULT };
